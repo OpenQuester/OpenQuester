4edbad3f8f68da2ca2760f948ea9f4b14c5f5cac
Asion001 <asion.desu@gmail.com>
2025-07-27 18:30:31 +0300

    chore: Gen files

--- client/lib/openquester.dart
+++ client/lib/openquester.dart
@@ -61,7 +61,6 @@ export 'src/features/game_lobby/view/game_lobby_screen.dart';
 export 'src/features/game_lobby/view/game_lobby_styles.dart';
 export 'src/features/game_lobby/view/game_lobby_theme.dart';
 export 'src/features/game_lobby/view/game_lobby_themes.dart';
-export 'src/features/game_lobby_editor/controllers/game_lobby_editor_controller.dart';
 export 'src/features/game_lobby_editor/view/game_lobby_editor.dart';
 export 'src/features/game_lobby_editor/view/game_lobby_editor_players.dart';
 export 'src/features/game_preview/controllers/game_preview_controller.dart';
