893f91e92b9f412106c2dec668a4504c72c9fab7
Asion <asion.desu@gmail.com>
2026-01-04 16:17:19 +0100

    fix: Add index_generator configuration check in GenerateIndexesTask

--- client/packages/project_helper/lib/tasks/gen_indexes_task.dart
+++ client/packages/project_helper/lib/tasks/gen_indexes_task.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:mason_logger/mason_logger.dart';
+import 'package:path/path.dart' as path;
 import 'package:project_helper/build_task.dart';
 import 'package:project_helper/utils.dart';
 
@@ -17,6 +20,15 @@ class GenerateIndexesTask implements BuildTask {
     Progress? progress,
     bool verbose = false,
   }) async {
+    // Check for index_generator configuration
+    final indexGeneratorConfig = File(
+      path.join(workingDirectory, 'index_generator.yaml'),
+    );
+    if (indexGeneratorConfig.existsSync() == false) {
+      logger.warn('⚠ No index_generator.yaml found, skipping...');
+      return true;
+    }
+
     if (verbose) logger.info('Generating Index Files');
 
     final command = getFlutterCommand();
